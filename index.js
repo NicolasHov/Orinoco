c1ca22f8106cb830cf4c5361f957e0d69878f0e9
Nicolas Hovart <hovart.nicolas@gmail.com>
2021-02-11 16:23:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// Appel et requete API 
+console.log('rerererre');
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        var response = JSON.parse(this.responseText);
+        console.log(response);
+    }
+};
+request.open("GET", "http://localhost:3000/api/teddies/");
+request.send();
