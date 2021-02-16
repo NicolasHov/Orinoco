4d97e33f6d172e3549cee06c9c22b1a33b832996
Nicolas Hovart <hovart.nicolas@gmail.com>
2021-02-17 00:41:51 +0100

    Create page Vue with one teddy

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 
-const buttonFirstTeddy = document.getElementById('ask-teddy')
-const teddyResultElt = document.getElementById('teddy-result')
 
 // Appel avec fetch (promise)
 async function getTeddiesData(){
     const apiUrl = "http://localhost:3000/api/teddies/"
 
     // promess = il promet quil va recup les infos, on ajoute "then" pour le chaînage et on définit la fonction directement dedans 
-    const data = await fetch(apiUrl)
+    const data = await fetch(apiUrl).then(
+        console.log("fetch done")
+        )
     const json = await data.json()
-    console.log(json)
+    // console.log(json)
     return json
 }
-
 
 
 async function displayProduct(event) {
@@ -21,19 +20,48 @@ async function displayProduct(event) {
     console.log("Evenement détecté");
 
 
-    // 1 récup nom de ville a aller chercher
-    // const cityName = inputElt.value
-    // console.log(cityName)
+    //TODO: récup teddy par id
+    // const teddyID = inputElt.value 
+    // console.log(teddyID)
 
-    // 2 récup info météo de cette ville
+    // 2 récup infos du teddy
     const teddiesData = await getTeddiesData()
-    console.log(teddiesData)
+    const teddyData =  teddiesData[0]
+
+    // get DOM elements
+    const imageTeddy = document.getElementById('imageURL')
+    const nameTeddy = document.getElementById('title')
+    const descriptionTeddy = document.getElementById('description')
+    const priceTeddy = document.getElementById('price')
+    const colors1Teddy = document.getElementById('colors1')
+    const colors2Teddy = document.getElementById('colors2')
+    const colors3Teddy = document.getElementById('colors3')
+
+    // populate DOM elements with data
+    nameTeddy.innerHTML = teddyData.name
+    descriptionTeddy.innerHTML = teddyData.description
+    priceTeddy.innerHTML = teddyData.price + "€"
+
+    // it have worked but dropdown wont open.apply.?
+    colors1Teddy.innerHTML = teddyData.colors[0]
+    colors2Teddy.innerHTML = teddyData.colors[1]
+    colors3Teddy.innerHTML = teddyData.colors[2]
+    
+
+    var src = document.createAttribute("src")
+    src.value = teddyData.imageUrl
+    imageTeddy.setAttributeNode(src);           
+    
+    // teddiesData[0].forEach(element => {
+    // });
 }
 
-buttonFirstTeddy.addEventListener('click', displayProduct)
+window.addEventListener("load", displayProduct) 
 
 
-// Appel avec XMLHttpRequest
+
+
+// ------ Appel avec XMLHttpRequest ------
 // var request = new XMLHttpRequest();
 // request.onreadystatechange = function() {
 //     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
